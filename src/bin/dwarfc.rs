9153115546b18e0ab768bdd2b4549c70c5d943c7
Keith T. Star <uberfoo@me.com>
2023-04-01 05:43:57 -0700

    added extensions
    I nearly forgot about this idea. Glad that I was reminded.

--- src/bin/dwarfc.rs
+++ src/bin/dwarfc.rs
@@ -2,6 +2,8 @@ use std::{env, fs};
 
 use sarzak::dwarf::parse;
 
+const EXTENSIONS: [&str; 2] = ["tao", "道"];
+
 fn main() {
     let src = fs::read_to_string(env::args().nth(1).expect("Expected file argument"))
         .expect("Failed to read file");
