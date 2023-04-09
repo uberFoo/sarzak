80eb28d09cda8946823ee7e443eaaf56c4c12e9c
Keith T. Star <uberfoo@me.com>
2023-04-09 08:06:38 -0700

    Added Lu Dog
    Modules and whatnot.

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod v2;
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub use v2::drawing;
+pub use v2::lu_dog;
 pub use v2::merlin;
 pub use v2::sarzak;
 pub use v2::woog;
--- src/v2.rs
+++ src/v2.rs
@@ -1,5 +1,6 @@
 pub mod domain;
 pub mod drawing;
+pub mod lu_dog;
 pub mod merlin;
 pub mod sarzak;
 pub mod woog;
